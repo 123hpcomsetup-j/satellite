50cd399bf83e06dbf68d93b084a76c03920c8b00
123.hp.com/setup <153689407+123hpcomsetup-j@users.noreply.github.com>
2025-08-27 00:23:54 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -166,6 +166,17 @@ export default function RootLayout({
             })();
           `}
         </Script>
+        <!-- Statcounter code for star
+http://www.starinternetca.site/ on Hostinger -->
+<script type="text/javascript">
+var sc_project=13161983; 
+var sc_invisible=1; 
+var sc_security="36fb2679"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<!-- End of Statcounter Code -->
       </head>
       <body className={inter.className}>{children}</body>
     </html>
