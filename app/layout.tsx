972d9a8e18e4830aa753f686b304576b4d9c95d7
123.hp.com/setup <153689407+123hpcomsetup-j@users.noreply.github.com>
2025-08-27 02:13:28 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -177,7 +177,7 @@ src="https://www.statcounter.com/counter/counter.js"
 async></script>
 
 <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17312772339" >
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-17312772339" strategy="afterInteractive" >
 </script>
 <script>
   window.dataLayer = window.dataLayer || [];
